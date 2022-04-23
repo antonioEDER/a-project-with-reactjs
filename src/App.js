8d6e7280b27e0aceb4509e917f2ca3c00ef7cf4d
antonioEDER <antonioeder007@gmail.com>
2022-04-23 17:13:42 -0400

    Refaturando Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
+import { Component } from 'react';
 import './App.css';
-import Comentatio from './components/Comentario';
-function App() {
-  return (
-    <div className="App">
-      <h1>Meu projeto</h1>
+import Comentario from './components/Comentario';
+// function App () {
+  
+class App extends Component {
+  state = {
+    comentarios: [
+      {
+        id: 123,
+        nome: "João",
+        email: "joao@gmail.com",
+        data: new Date(),
+        mensagem: "Olá, tudo bem?"
+      },
+      {
+        id: 456,
+        nome: "Maria",
+        email: "maria@gmail.com",
+        data: new Date(),
+        mensagem: "Estou bem! E você?"
+      }
+    ]
+  }
 
-      <Comentatio />
-      
-    </div>
-  );
+  adicionarComentarios = () => {
+    const comentario = {
+        id: Math.random(),
+        nome: "Ana",
+        email: "ana@gmail.com",
+        data: new Date(),
+        mensagem: "Olá, tudo bem?"
+    }
+
+    // let lista = this.state.comentarios
+    // lista.push(comentario
+    // this.setState({ comentarios: lista})
+
+    this.setState({
+      comentarios: [ ...this.state.comentarios, comentario]
+    })
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Meu projeto</h1>
+
+        <Comentario nome="Pedro" email="Pedro@gmail.com" data={new Date()}>
+          Olá, tudo bem? [props.children]
+        </Comentario>
+
+        {
+          this.state.comentarios.map((comentario, indice) => (
+            <Comentario
+              nome={comentario.nome}
+              email={comentario.email}
+              data={comentario.data}
+              key={comentario.id}
+            />
+          ))
+        }
+        <button onClick={this.adicionarComentarios}>Adicionar comentário</button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Comentario.js
+++ src/components/Comentario.js
@@ -1,11 +1,34 @@
 import React from "react";
 import './Comentario.css'
 
-const Comentario = () => (
-    <div className="Comentario">
-        <div>João</div>
-        <div>Olá, tudo bem?</div>
-    </div>
-);
+// const Comentario = () => (
+//     <div className="Comentario">
+//         <div>João</div>
+//         <div>Soma { 2 + 5 }</div>
+//     </div>
+// );
 
+
+// const Comentario = () => {
+//     const a = 5;
+//     const b = 5
+//     return (
+//         <div className="Comentario">
+//             <div>João</div>
+//             <div>Soma {a + b}</div>
+//         </div>
+//      )
+// };
+
+
+const Comentario = props => {
+    return (
+        <div className="Comentario">
+            <div>{ props.nome }</div>
+            <div>{ props.email }</div>
+            <div>{ props.data.toString() }</div>
+            <div>{ props.children }</div>
+        </div>
+     )
+};
 export default Comentario;
